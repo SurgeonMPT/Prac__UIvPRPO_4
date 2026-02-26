ad83f2a422f24b5cba16dcce95f7e26844a8fb77
SurgeonMPT <47532294+SurgeonMPT@users.noreply.github.com>
2026-02-26 23:15:22 +0300

    Снова исправление

--- src/PhoneNormalizer.java
+++ src/PhoneNormalizer.java
@@ -13,8 +13,8 @@ public class PhoneNormalizer {
             m.appendReplacement(sb, Matcher.quoteReplacement(normalized));
         }
         m.appendTail(sb);
-        // return sb.toString();
-        return
+        return sb.toString();
+        // return
     }
 
     private String normalizeWhitespace(String s) {
